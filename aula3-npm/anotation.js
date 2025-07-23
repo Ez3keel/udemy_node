07834a8a916ffbbfe2a8f1a41736234d44109e56
Ezequiel <ezequiel.zunesha@gmail.com>
2025-07-23 10:01:30 -0300

    aula 3

---
+++ aula3-npm/anotation.js
@@ -0,0 +1,50 @@
+/*
+mover o express para devDependencies = 
+    npm i express --save-dev
+
+mover para dependencies = 
+    npm i express --save-prod 
+
+como instalar uma versão especifica do express = 
+    npm i express@2.1.0
+
+Atualizar dependencias do projeto
+    npm update
+
+
+
+
+^5.1.0
+
+quando tem o ^ ele permite atualização na minor e na patch
+
+5.    1.     0 
+major minor patch
+
+
+outra forma de atualizar seria colocando assim 
+    npm install express@4.x
+
+    assim ele vai atualizar o minor e o patch de acordo com a versão
+
+
+    Desinstalar
+    npm uninstall express
+
+
+    Listar pacotes instalados
+    npm ls
+
+    Ver somente as dependencias que você instalou
+    npm ls --depth=0
+    npm ls --depth=1 
+
+
+    ver qual pacotes estão desatualizados
+    npm outdated
+
+
+
+    
+
+*/
